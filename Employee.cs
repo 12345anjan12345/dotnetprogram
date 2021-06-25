42968d7f3e6548198b1d68fed311220d8dcf75c8
12345anjan12345 <84557654+12345anjan12345@users.noreply.github.com>
2021-06-25 11:26:25 +0545

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EmployeeDisplay
+{
+    class Employee
+    {
+        string firstName;
+        string lastName;
+        double salary;
+
+        public Employee(string first_name, string last_name, double salary)
+        {
+            firstName = first_name;
+            lastName = last_name;
+            this.salary = salary;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary
+        {
+            get => salary;
+            set => salary = value;
+        }
+
+        public string FullName => firstName + " " + lastName;
+
+        public double IncrementSalary(double s)
+        {
+            double increment = (s / 100) * salary;
+            salary = salary + increment;
+
+            return increment;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Name: \"" + FullName + "\" | Salary: " + salary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("Anjan", "Ghimire", 2000000);
+            Console.WriteLine("Name: " + e1.FullName);
+            Console.WriteLine("Salary: " + e1.Salary);
+            Console.WriteLine("\nSalary incremeneted by(10%): " + e1.IncrementSalary(10) + "\n");
+            e1.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
